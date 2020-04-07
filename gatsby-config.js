728cc2d8e601802a325948923a54acc1a5d5ff99
nicopresto <nicopresto@gmail.com>
2020-04-06 23:47:37 -0700

    revises path to markdown

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,7 +9,7 @@ module.exports = {
     resolve: `gatsby-source-filesystem`,
     options: {
       name: `markdown-pages`,
-      path: `${__dirname}/content/posts`
+      path: `${__dirname}/content/posts/blog`
     },
   },
   ],
